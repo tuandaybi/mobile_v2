959d3837833e3ac2a60b87492c80ea65d1f23845
Anh Tuan <tuanda.ybi@gmail.com>
2026-04-30 11:33:23 +0700

    Fix app update delete route

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -9,3 +9,4 @@ Route::get('/uploader', [AppUpdateController::class, 'dashboard']);
 
 Route::post('/request-download-otp', [AppUpdateController::class, 'requestDownloadOtp'])->name('app-updates.request-otp');
 Route::post('/verify-download-otp', [AppUpdateController::class, 'verifyDownloadOtp'])->name('app-updates.verify-otp');
+Route::post('/delete-file', [AppUpdateController::class, 'deleteWithOtp'])->name('app-updates.delete-with-otp');
